6ba54bd3796b9dcd7188390a8c2a735a7f603cd4
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-29 03:56:28 -0800

    added an app route to the metadata

--- app.py
+++ app.py
@@ -11,13 +11,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    message =  "Bart Ridership in 2018"
-    return render_template("index.html", message = message)
+    title =  "Bart Ridership in 2018"
+    return render_template("index.html", title = title)
 
 @app.route('/data')
 def data():
     cursor = db.cursor()
     sql = "SELECT * FROM ridership"
+    cursor.execute(sql)
+
+    # gets the column headers in the merged table
+    columns = [col[0] for col in cursor.description] 
+
+    # output is a list of dictionaries (key: column header, value: data)
+    results = [dict(zip(columns, row)) for row in cursor.fetchall()] 
+
+    # json format for list of dictionaries
+    return jsonify (results)
+    session.close
+
+@app.route('/metadata')
+def metadata():
+    cursor = db.cursor()
+    sql = "SELECT * FROM metadata"
     cursor.execute(sql)
 
     # gets the column headers in the merged table
