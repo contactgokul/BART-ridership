18669529698fed3857c319f0395a4c4e4908ff07
Rochie Cuevas <rochiecuevas@gmail.com>
2019-01-29 03:56:52 -0800

    added a SQL table called metadata

--- toMySql.sql
+++ toMySql.sql
@@ -1,9 +1,9 @@
 -- Create and use bart_db
-DROP DATABASE IF EXISTS bart_db;
+-- DROP DATABASE IF EXISTS bart_db;
 CREATE DATABASE bart_db;
 USE bart_db;
 
--- Create table for raw data to be loaded into
+-- Create ridership table for raw data to be loaded into
 CREATE TABLE ridership (
 ID FLOAT PRIMARY KEY,
 Entry_Station TEXT,
@@ -14,5 +14,21 @@ Yr_Mo DATE,
 Avg_Weekday_Trips DECIMAL(2)
 );
 
+-- Create metadata table for raw data to be loaded into 
+CREATE TABLE metadata (
+ID float primary key,
+abbr text,
+address text,
+city text,
+county text,
+gtfs_latitude float,
+gtfs_longitude float,
+`name` text,
+state text,
+zipcode text,
+abbr2 text
+);
+
 -- Select all data
 select * from ridership;
+select * from metadata;
