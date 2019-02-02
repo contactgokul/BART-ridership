d968176dd6eb58497b68116de794aed908732918
Lumeng Bai <lumengbai@Lumengs-MacBook-Pro-2.local>
2019-02-01 21:28:48 -0800

    Created stations,py

---
+++ stations.py
@@ -0,0 +1,137 @@
+STATION_LIST = [{
+        'station_code':'RM',
+        'station_name':"Richmond"
+    },{
+        'station_code':'EN',
+        'station_name':"El Cerrito Del Norte"
+    },{
+        'station_code':'EP',
+        'station_name':"El Cerrito Plaza"
+    },{
+        'station_code':'NB',
+        'station_name':"North Berkeley"
+    },{
+        'station_code':'BK',
+        'station_name':"Berkeley"
+    },{
+        'station_code':'AS',
+        'station_name':"Ashby"
+    },{
+        'station_code':'MA',
+        'station_name':"MacArthur"
+    },{
+        'station_code':'19',
+        'station_name':"19th Street Oakland"
+    },{
+        'station_code':'LM',
+        'station_name':"Lake Merritt"
+    },{
+        'station_code':'FV',
+        'station_name':"Fruitvale"
+    },{
+        'station_code':'CL',
+        'station_name':"Coliseum"
+    },{
+        'station_code':'SL',
+        'station_name':"San Leandro"
+    },{
+        'station_code':'BF',
+        'station_name':"Bayfair"
+    },{
+        'station_code':'HY',
+        'station_name':"Hayward"
+    },{
+        'station_code':'SH',
+        'station_name':"South Hayward"
+    },{
+        'station_code':'UC',
+        'station_name':"Union City"
+    },{
+        'station_code':'FM',
+        'station_name':"Fremont"
+    },{
+        'station_code':'CN',
+        'station_name':"Concord"
+    },{
+        'station_code':'PH',
+        'station_name':"Pleasant Hill"
+    },{
+        'station_code':'WC',
+        'station_name':"Walnut Creek"
+    },{
+        'station_code':'LF',
+        'station_name':"Lafayette"
+    },{
+        'station_code':'OR',
+        'station_name':"Orinda"
+    },{
+        'station_code':'RR',
+        'station_name':"Rockridge"
+    },{
+        'station_code':'OW',
+        'station_name':"West Oakland"
+    },{
+        'station_code':'EM',
+        'station_name':"Embarcadero"
+    },{
+        'station_code':'MT',
+        'station_name':"Montgomery Street"
+    },{
+        'station_code':'PL',
+        'station_name':"Powell Street"
+    },{
+        'station_code':'CC',
+        'station_name':"Civic Center"
+    },{
+        'station_code':'16',
+        'station_name':"16th Street Mission"
+    },{
+        'station_code':'24',
+        'station_name':"24th Street Mission"
+    },{
+        'station_code':'GP',
+        'station_name':"Glen Park"
+    },{
+        'station_code':'BP',
+        'station_name':"Balboa Park"
+    },{
+        'station_code':'DC',
+        'station_name':"Daly City"
+    },{
+        'station_code':'CM',
+        'station_name':"Colma"
+    },{
+        'station_code':'CV',
+        'station_name':"Castro Valley"
+    },{
+        'station_code':'ED',
+        'station_name':"Dublin/Pleasanton"
+    },{
+        'station_code':'NC',
+        'station_name':"North Concord"
+    },{
+        'station_code':'WP',
+        'station_name':"Pittsburg/Bay Point"
+    },{
+        'station_code':'SS',
+        'station_name':"South San Francisco"
+    },{
+        'station_code':'SB',
+        'station_name':"San Bruno"
+    },{
+        'station_code':'SO',
+        'station_name':"San Francisco International Airport" 
+    },{
+        'station_code':'MB',
+        'station_name':"Millbrae" 
+    },{
+        'station_code':'WD',
+        'station_name':"West Dublin/Pleasanton" 
+    },{
+        'station_code':'OA',
+        'station_name':"Oakland International Airport"
+    },{
+        'station_code':'WS',
+        'station_name':"Warm Springs"
+    }
+    ]
